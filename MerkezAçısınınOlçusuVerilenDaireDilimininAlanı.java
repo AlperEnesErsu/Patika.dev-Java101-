b5770828521280714282d966a39faa7729b8af98
Alper Enes Ersü <alperenesersu.b@gmail.com>
2025-05-17 10:09:31 +0300

    Merkez Açısının Ölçüsü Verilen Daire Diliminin Alanı

---
+++ MerkezAçısınınOlçusuVerilenDaireDilimininAlanı.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class MerkezAçısınınOlçusuVerilenDaireDilimininAlanı {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		double pi = Math.PI;
+		
+		System.out.print("Lütfen Daire Diliminin Yarıçapını Giriniz : ");
+		double r = scanner.nextDouble();
+		
+		System.out.print("Lütfen Daire Diliminin Merkez Açısının Ölçüsünü Giriniz : ");
+		double angle = scanner.nextDouble();
+		
+		double formula = (pi*(r*r)*angle)/360;
+		
+		System.out.println("Yarıçap Değeriniz : " + r);
+		System.out.println("Açı Değeriniz : " + angle);
+		System.out.println("Daire Diliminin Alanı : " + formula);
+		
+	}
+
+}
